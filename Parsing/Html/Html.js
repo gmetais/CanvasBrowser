473a53178f0b47842aa502b6d468f2b40300058b
gmetais <gael.metais@gmail.com>
2012-05-29 17:31:45 +0300

    Update Parsing/Html/Html.js

--- Parsing/Html/Html.js
+++ Parsing/Html/Html.js
@@ -5,36 +5,86 @@ CanvasBrowser.Parsing.Html.init = function() {
 
 CanvasBrowser.Parsing.Html.parse = function(html) {
 	
-	var results = "";
+	var windowObject = {};
+	var cursor = null;
 	
 	// Convert the HTML to XML ( = strict HTML)
 	HTMLParser(html, {
-		start: function( tag, attrs, unary ) {
-			results += "<" + tag;
+		start: function(tagName, attrs, unary) {
+			/*results += "<" + tag;
 	
 			for ( var i = 0; i < attrs.length; i++ )
 				results += " " + attrs[i].name + '="' + attrs[i].escaped + '"';
 	
-			results += (unary ? "/" : "") + ">";
+			results += (unary ? "/" : "") + ">";*/
+			
+			tagName = tagName.toUpperCase();
+			
+			// If it's the first element, we add it to the windowObject
+			if (windowObject.length == 0 && tagName != "HTML") {
+				windowObject.document = new HTMLElement("HTML");
+				cursor = windowObject.document;
+			}
+			
+			var element = new HTMLElement(tagName, attrs);
+			cursor.appendChild(element);
+			
+			// If the tag is unary (can't have any child), no need to move the cursor
+			if (!unary) {
+				cursor = element;
+			}
 		},
-		end: function( tag ) {
-			results += "</" + tag + ">";
+		end: function(tagName) {
+			/*results += "</" + tag + ">";*/
+			if (cursor.getTagName() == tagName) {
+				cursor = cursor.getParentNode();
+			} else {
+				throw "DOM construction error";
+			}
 		},
 		chars: function( text ) {
+			// Delete useless duplicated blank chars
 			text = text.replace(/\s+/, " ");
-			
-			// TODO : deal with HTML special chars that throw a parsing error (ex: &orcic; = ô)
 			
 			results += text;
 		},
 		comment: function( text ) {
-			results += "<!--" + text + "-->";
+			//results += "<!--" + text + "-->";
 		}
 	});
 	
-	console.log(results);
+	console.log(dom);
 	
 	
 	
 	
+};
+
+HTMLElement = function(tagName, attrs) {
+	this.tagName = tagName;
+	if (attrs) {
+		this.attributes = attrs;
+	}
+	
+};
+
+HTMLElement.prototype = {
+	tagName : '',
+	attributes : [],
+	
+	parentNode : '',
+	childNodes : [],
+	
+	appendChild : function(htmlElement) {
+		this.childNodes[] = htmlElement;
+		htmlElement.setParentNode = this;
+	},
+	
+	getTagName : function() {
+		return this.tagName;
+	},
+	
+	getParentNode : function() {
+		return this.parentNode;
+	}
 };
